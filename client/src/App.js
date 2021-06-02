2fe09de2ad3e53baf6d02c2e3434242148bab605
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2021-06-02 19:48:02 +0530

    added about

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
-import Header from './Components/Header/Header';
-import Mission from './Components/Mission/Mission';
-import Journey from './Components/Journey/Journey';
-import Rehabilitation from './Components/Rehabilitation/Rehabilitation';
+// import Header from './Components/Header/Header';
+// import Mission from './Components/Mission/Mission';
+// import Journey from './Components/Journey/Journey';
+// import Rehabilitation from './Components/Rehabilitation/Rehabilitation';
 import About from './Components/About/About';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Rehabilitation />
-      <Mission />
-      <Journey />
       <About />
     </div>
   );

2d078cdfb11704d5edb2ba36ea51144eb5458c4f
ar30612 <ar30612@gmail.com>
2021-06-06 16:37:55 +0200

    AboutUsImages added

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 // import Mission from './Components/Mission/Mission';
 // import Journey from './Components/Journey/Journey';
 // import Rehabilitation from './Components/Rehabilitation/Rehabilitation';
-// import About from './Components/About/About';
+import About from './Components/About/About';
 import AboutUs from './Components/aboutUs_shadab/about_us';
 import AboutComponent from './Components/AboutUs/AboutComponent';
 import CarouselComponent from './Components/Carousel/CarouselComponent';
@@ -10,8 +10,8 @@ function App() {
   return (
     <div>
     <AboutComponent/>
-      {/* <About /> */}
       <AboutUs />
+      <About /> 
       <CarouselComponent/>
     </div>
   );
